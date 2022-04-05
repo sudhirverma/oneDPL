c457f6c15f0a49ffcebe000e9299584ce91f108f
Sergey Kopienko <sergey.kopienko@intel.com>
2022-04-05 09:55:21 +0300

    sycl_iterator.pass.cpp - test_set_union - finish test when n = full_size

--- test/general/sycl_iterator.pass.cpp
+++ test/general/sycl_iterator.pass.cpp
@@ -3605,7 +3605,7 @@ DEFINE_TEST(test_set_union)
     operator()(Policy&& exec, Iterator1 first1, Iterator1 last1, Iterator2 first2, Iterator2 last2, Iterator3 first3,
                Iterator3 last3, Size n)
     {
-        assert(n >= full_size);
+        assert(n == full_size);
 
         TestDataTransfer<UDTKind::eKeys, Size> host_keys(*this, n);
         TestDataTransfer<UDTKind::eVals, Size> host_vals(*this, n);
@@ -3833,7 +3833,9 @@ test_usm_and_buffer()
     PRINT_DEBUG("test_set_symmetric_difference");
     test3buffers<alloc_type, test_set_symmetric_difference<ValueType>, full_size /* start test from n = full_size */>();
     PRINT_DEBUG("test_set_union");
-    test3buffers<alloc_type, test_set_union<ValueType>, full_size /* start test from n = full_size */>();
+    test3buffers<alloc_type, test_set_union<ValueType>,
+                 full_size /* start test from n = full_size */,
+                 full_size /* continue test while n <= full_size */>();
     PRINT_DEBUG("test_set_difference");
     test3buffers<alloc_type, test_set_difference<ValueType>, full_size /* start test from n = full_size */>();
     PRINT_DEBUG("test_set_intersection");
