b143a634a64b12140a2805ff311ec4323fa896bc
Dan Hoeflinger <dan.hoeflinger@intel.com>
2022-12-06 10:16:05 -0500

    test commit addin comment Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
@@ -153,6 +153,8 @@ __radix_sort_count_submit(_ExecutionPolicy&& __exec, ::std::size_t __segments, :
 #endif
 )
 {
+    //adding comment for test away from formatting issues.
+
     // typedefs
     using _ValueT = oneapi::dpl::__internal::__value_t<_ValRange>;
     using _CountT = typename _CountBuf::value_type;
